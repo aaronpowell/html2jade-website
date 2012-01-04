a705e7a914380a779a869530a0c75254f28f66ec
Aaron Powell <me@aaron-powell.com>
2012-01-04 21:31:10 +1100

    basic UI and wireup for the page

--- static/app.js
+++ static/app.js
@@ -1,7 +1,18 @@
 (function (exports) {
+	'use strict';
+
 	exports.convert = function (html, cb) {
-		$.post('/convert', { html: html }, function (jade) {
-			console.log(arguments);
-		});
+		$.post('/convert', { html: html }, cb);
 	};
+
+    $('form').submit(function (e) {
+        e.preventDefault();
+
+        var form = $(this);
+        var html = form.find('#html').val();
+
+        exports.convert(html, function (result) {
+            form.find('#jade').val(result.jade);
+        });
+    });
 }).call(window, window.app = {});
